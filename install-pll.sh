f02623fb1a7e3975a81bc9713844c70a9dd1486a
Alex Safatli <safatli@cs.dal.ca>
2015-03-04 09:59:41 -0400

    Fixed Travis install script.

--- install-pll.sh
+++ install-pll.sh
@@ -2,4 +2,6 @@
 LIBPLL=libpll-1.0.2-sse3-64
 wget http://libpll.org/Downloads/${LIBPLL}.tar.gz
 tar -xvf ${LIBPLL}.tar.gz
-cd ${LIBPLL} && cp libpll* /usr/local/lib/ && cp -r include/pll /usr/local/include/
+cd ${LIBPLL} && sudo cp libpll* /usr/local/lib/ && sudo cp -r include/pll /usr/local/include/
+export LN_LIBRARY_PATH=$LN_LIBRARY_PATH:/usr/local/lib/
+
